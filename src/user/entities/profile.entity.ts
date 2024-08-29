e9f9328ad0b7d43d63260fd07af399860bffed15
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-08-29 17:35:05 +0330

    create ProfileEntity

---
+++ src/user/entities/profile.entity.ts
@@ -0,0 +1,31 @@
+import { BaseEntity } from "src/common/abstracts/base.entity";
+import { EntityName } from "src/common/enums/entity.enum";
+import { Column, Entity } from "typeorm";
+
+@Entity(EntityName.Profile)
+export class ProfileEntity extends BaseEntity {
+  @Column()
+  nick_name: string;
+
+  @Column({ nullable: true })
+  bio: string;
+
+  @Column({ nullable: true })
+  profile_image: string;
+
+  @Column({ nullable: true })
+  bg_image: string;
+
+  @Column({ nullable: true })
+  gender: string;
+
+  @Column({ nullable: true })
+  birthday: Date;
+
+  // Social Media
+  @Column({ nullable: true })
+  linkedin_profile: string;
+
+  @Column({ nullable: true })
+  x_profile: string;
+}
