faede7f0970afea49e7b833c19e690b46bd84141
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-08-29 17:34:43 +0330

    create UserEntity

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,25 @@
+import { BaseEntity } from "src/common/abstracts/base.entity";
+import { EntityName } from "src/common/enums/entity.enum";
+import { Column, CreateDateColumn, Entity } from "typeorm";
+
+@Entity(EntityName.User)
+export class UserEntity extends BaseEntity {
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true, nullable: true })
+  email: string;
+
+  @Column({ unique: true, nullable: true })
+  mobile: string;
+
+  @Column({ nullable: true })
+  password: string;
+
+  //  Date & time
+  @CreateDateColumn()
+  created_at: Date;
+
+  @CreateDateColumn()
+  updated_at: Date;
+}
