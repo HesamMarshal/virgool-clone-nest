a2d11671556071987d0001888742925554b2578b
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-08-29 13:52:35 +0330

    activate swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { SwaggerConfigInit } from "./config/swagger.config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Confgure Swagger
+  SwaggerConfigInit(app);
+
+  // Start Server
+  await app.listen(3000, () => {
+    console.log("http://localhost:3000");
+  });
 }
 bootstrap();
