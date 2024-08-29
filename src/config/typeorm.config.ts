5993427137003e7930374e9c8c3c58cb716ee870
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-08-29 14:25:26 +0330

    add typeormconfig

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export function TypeOrmConfig(): TypeOrmModuleOptions {
+  const { DB_HOST, DB_PORT, DB_NAME, DB_USERNAME, DB_PASSWORD } = process.env;
+  return {
+    type: "postgres",
+    host: DB_HOST,
+    port: +DB_PORT,
+    database: DB_PASSWORD,
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+
+    autoLoadEntities: false,
+
+    synchronize: true,
+    entities: [
+      "dist/**/**/**/*.entity{.ts,.js}",
+      "dist/**/**/*.entity{.ts,.js}",
+    ],
+  };
+}
