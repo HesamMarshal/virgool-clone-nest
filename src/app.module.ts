98304df125cca1ea9a1dc7f3ec14f5f6b289398b
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-08-29 14:52:38 +0330

    add typeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ConfigModule } from "@nestjs/config";
 import { join } from "path";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { TypeOrmConfig } from "./config/typeorm.config";
 
 @Module({
   imports: [
@@ -10,6 +12,7 @@ import { join } from "path";
       isGlobal: true,
       envFilePath: join(process.cwd(), ".env"),
     }),
+    TypeOrmModule.forRoot(TypeOrmConfig()),
   ],
   controllers: [AppController],
   providers: [AppService],
