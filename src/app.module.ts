2486b05c88fd7861b116ace2abb5c1e65723bcbf
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-08-29 17:12:55 +0330

    create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from "@nestjs/config";
 import { join } from "path";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TypeOrmConfig } from "./config/typeorm.config";
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { TypeOrmConfig } from "./config/typeorm.config";
       envFilePath: join(process.cwd(), ".env"),
     }),
     TypeOrmModule.forRoot(TypeOrmConfig()),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
