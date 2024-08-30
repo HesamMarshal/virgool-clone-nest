1d7e7ac03b3e51460b35135d709dad592ee13a0b
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-08-30 10:57:05 +0330

    add some messages

--- src/common/enums/message.enum.ts
+++ src/common/enums/message.enum.ts
@@ -1,4 +1,10 @@
-export enum BadRequestMessage {}
-export enum AuthMessage {}
+export enum BadRequestMessage {
+  InValidLoginData = "اطلاعات ارسال شده برای ورود صحیح نمی باشد",
+  InValidRegisterData = "اطلاعات ارسال شده برای ثبت نام صحیح نمی باشد",
+}
+export enum AuthMessage {
+  NotFoundAccount = "حساب کاربری یافت نشد",
+  AlreadyExistUser = "حساب کاربری با این مشخصات وجود دارد",
+}
 export enum NotFoundMessage {}
 export enum ValidationMessage {}
