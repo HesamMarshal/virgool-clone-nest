cba33190ae77b27a7749320ec1390a1998383a90
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-09-01 12:38:12 +0330

    set type for payload

--- src/modules/auth/tokens.service.ts
+++ src/modules/auth/tokens.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { CookePayload } from "./types/payloads";
 
 @Injectable()
 export class TokenService {
   constructor(private jwtService: JwtService) {}
 
-  createOtpToken(payload: any) {
+  createOtpToken(payload: CookePayload) {
     const token = this.jwtService.sign(payload, {
       secret: process.env.OTP_TOKEN_SECRET,
       expiresIn: 120,
