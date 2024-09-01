2e9593e49b3e7172bdced18f783c5e22bbb0fb9e
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-09-01 13:53:57 +0330

    add route for checkotp

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Body, Controller, Post, Res } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { ApiConsumes, ApiTags } from "@nestjs/swagger";
-import { AuthDto } from "./dto/auth.dto";
+import { AuthDto, CheckOtpDto } from "./dto/auth.dto";
 import { SwaggerConsumes } from "src/common/enums/swagger-consume.enum";
 import { Response } from "express";
 
@@ -10,10 +10,15 @@ import { Response } from "express";
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  // Methods
   @Post("user-existence")
   @ApiConsumes(SwaggerConsumes.UrlEncoded, SwaggerConsumes.Json)
   userExistence(@Body() authDto: AuthDto, @Res() res: Response) {
     return this.authService.userExistence(authDto, res);
+  }
+
+  @Post("check-otp")
+  @ApiConsumes(SwaggerConsumes.UrlEncoded, SwaggerConsumes.Json)
+  checkOtp(@Body() checkOtpDto: CheckOtpDto) {
+    return this.authService.checkOTP(checkOtpDto.code);
   }
 }
