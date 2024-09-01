ed4867b313cafbefbae7c6e3223ceb003f1b25a6
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-09-01 12:40:49 +0330

    send response to the service

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, Res } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { ApiConsumes, ApiTags } from "@nestjs/swagger";
 import { AuthDto } from "./dto/auth.dto";
 import { SwaggerConsumes } from "src/common/enums/swagger-consume.enum";
+import { Response } from "express";
 
 @Controller("auth")
 @ApiTags("Auth")
@@ -12,7 +13,7 @@ export class AuthController {
   // Methods
   @Post("user-existence")
   @ApiConsumes(SwaggerConsumes.UrlEncoded, SwaggerConsumes.Json)
-  userExistence(@Body() authDto: AuthDto) {
-    return this.authService.userExistence(authDto);
+  userExistence(@Body() authDto: AuthDto, @Res() res: Response) {
+    return this.authService.userExistence(authDto, res);
   }
 }
