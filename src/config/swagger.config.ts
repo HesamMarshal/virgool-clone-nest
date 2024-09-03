d344a99189f010d3b11a2e03dedbc02c82979227
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-09-03 16:03:35 +0330

    add swagger config to send bearer

--- src/config/swagger.config.ts
+++ src/config/swagger.config.ts
@@ -1,13 +1,24 @@
-import { INestApplication } from "@nestjs/common";
+import { INestApplication, UseGuards } from "@nestjs/common";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { SecuritySchemeObject } from "@nestjs/swagger/dist/interfaces/open-api-spec.interface";
 
 export function SwaggerConfigInit(app: INestApplication): void {
   const document = new DocumentBuilder()
     .setTitle("Virgool-Clone")
     .setDescription("Virgol Clone Backend")
     .setVersion("v0.0.1")
+    .addBearerAuth(SwaggerAuthConfig(), "Authorization")
     .build();
 
   const swaggerDocumet = SwaggerModule.createDocument(app, document);
   SwaggerModule.setup("/swagger", app, swaggerDocumet);
+}
+
+function SwaggerAuthConfig(): SecuritySchemeObject {
+  return {
+    type: "http",
+    bearerFormat: "JWT",
+    in: "header",
+    scheme: "bearer",
+  };
 }
