f85335d490c51cf23b7097cae611e83268362410
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-09-03 13:47:21 +0330

    add a route to check-login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,9 @@
-import { Body, Controller, Post, Res } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req, Res } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { ApiConsumes, ApiTags } from "@nestjs/swagger";
 import { AuthDto, CheckOtpDto } from "./dto/auth.dto";
 import { SwaggerConsumes } from "src/common/enums/swagger-consume.enum";
-import { Response } from "express";
+import { Response, Request } from "express";
 
 @Controller("auth")
 @ApiTags("Auth")
@@ -20,5 +20,10 @@ export class AuthController {
   @ApiConsumes(SwaggerConsumes.UrlEncoded, SwaggerConsumes.Json)
   checkOtp(@Body() checkOtpDto: CheckOtpDto) {
     return this.authService.checkOTP(checkOtpDto.code);
+  }
+
+  @Get("check-login")
+  checkLogin(@Req() req: Request) {
+    return req.user;
   }
 }
