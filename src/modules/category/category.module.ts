d12e256a94ff80311a874f1da8a11bab5c79baba
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-09-03 16:22:54 +0330

    import AuthModule, UserModule, CategoryEntity

--- src/modules/category/category.module.ts
+++ src/modules/category/category.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
-import { CategoryService } from './category.service';
-import { CategoryController } from './category.controller';
+import { Module } from "@nestjs/common";
+import { CategoryService } from "./category.service";
+import { CategoryController } from "./category.controller";
+import { CategoryEntity } from "./entities/category.entity";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AuthModule } from "../auth/auth.module";
+import { UserModule } from "../user/user.module";
 
 @Module({
+  imports: [AuthModule, UserModule, TypeOrmModule.forFeature([CategoryEntity])],
   controllers: [CategoryController],
   providers: [CategoryService],
 })
