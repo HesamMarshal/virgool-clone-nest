dc1591a318f014098525ef4b36dc199b0bbec9e9
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-09-03 16:30:55 +0330

    create columns

--- src/modules/category/entities/category.entity.ts
+++ src/modules/category/entities/category.entity.ts
@@ -1,1 +1,12 @@
-export class Category {}
+import { BaseEntity } from "src/common/abstracts/base.entity";
+import { EntityName } from "src/common/enums/entity.enum";
+import { Column, Entity } from "typeorm";
+
+@Entity(EntityName.Category)
+export class CategoryEntity extends BaseEntity {
+  @Column()
+  title: string;
+
+  @Column({ nullable: true })
+  priority: number;
+}
