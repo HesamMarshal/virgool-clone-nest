889376b3dc69334180a8cd1ea0205cdd6caf8758
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-09-05 16:19:23 +0330

    add endpoint

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -7,24 +7,58 @@ import {
   Param,
   Delete,
   Put,
+  UseInterceptors,
+  UploadedFiles,
+  ParseFilePipe,
+  UseGuards,
 } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
-import { ApiConsumes, ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiConsumes, ApiTags } from "@nestjs/swagger";
 import { ProfileDto } from "./dto/profile.dto";
 import { SwaggerConsumes } from "src/common/enums/swagger-consume.enum";
+import { FileFieldsInterceptor } from "@nestjs/platform-express";
+import { diskStorage } from "multer";
+import {
+  multerDestination,
+  multerFilename,
+} from "src/common/utils/multer.util";
+import { AuthGuard } from "../auth/guards/auth.guard";
+import { ProfileImages } from "./types/files.type";
 
 @Controller("user")
 @ApiTags("User")
+@ApiBearerAuth("Authorization")
+@UseGuards(AuthGuard)
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   // TODO: changeProfile to updateProfile
   @Put("/profile")
   @ApiConsumes(SwaggerConsumes.MultipartData)
-  changeProfile(@Body() profileDto: ProfileDto) {
-    return this.userService.changeProfile(profileDto);
+  // @UseInterceptors(FileInterceptor("bg_image"))
+  @UseInterceptors(
+    FileFieldsInterceptor(
+      [
+        { name: "profile_image", maxCount: 1 },
+        { name: "bg_image", maxCount: 1 },
+      ],
+      {
+        storage: diskStorage({
+          destination: multerDestination("user-profile"),
+          filename: multerFilename,
+        }),
+      }
+    )
+  )
+  changeProfile(
+    @UploadedFiles(new ParseFilePipe({ fileIsRequired: false, validators: [] }))
+    files: ProfileImages,
+    @Body()
+    profileDto: ProfileDto
+  ) {
+    return this.userService.changeProfile(files, profileDto);
   }
 
   // @Post()
