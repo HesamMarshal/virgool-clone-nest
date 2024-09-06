7a30ab8b824d92ffdc837a21b5ae71621adb6e68
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-09-06 20:16:48 +0330

    add changePhone and verifyPhone

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -168,6 +168,55 @@ export class UserService {
       // accessToken,
     };
   }
+  async changePhone(phone: string) {
+    const { id } = this.request.user;
+    const user = await this.userRepository.findOneBy({ phone });
+    if (user && user.id !== id)
+      throw new ConflictException(UserMessage.ConflictPhone);
+    else if (user && user.id === id) {
+      return { message: PublicMessage.Updated };
+    }
+
+    await this.userRepository.update({ id }, { new_phone: phone });
+    const otp = await this.authService.saveOTP(id, AuthMethod.Phone);
+    const token = await this.tokenService.createPhoneToken({ phone });
+    return {
+      code: otp.code,
+      token,
+    };
+  }
+
+  async verifyPhone(code: string) {
+    const { id: userId, new_phone } = this.request.user;
+
+    const token = this.request.cookies?.[CookieKeys.PhoneOTP];
+    console.log(token);
+
+    if (!token) throw new BadRequestException(AuthMessage.ExpiredCode);
+    const { phone } = this.tokenService.verifyPhoneToken(token);
+
+    if (phone !== new_phone)
+      throw new BadRequestException(BadRequestMessage.SomethingWrong);
+
+    const otp = await this.checkOtp(userId, code);
+
+    if (otp.method !== AuthMethod.Phone)
+      throw new BadRequestException(BadRequestMessage.SomethingWrong);
+
+    await this.userRepository.update(
+      { id: userId },
+      {
+        phone,
+        verify_phone: true,
+        new_phone: null,
+      }
+    );
+
+    return {
+      message: UserMessage.Updated,
+      // accessToken,
+    };
+  }
 
   async checkOtp(userId: number, code: string) {
     const otp = await this.otpRepository.findOneBy({ userId });
