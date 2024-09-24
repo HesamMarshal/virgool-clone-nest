ce523f1f60e29cf4b4ba7a22df837876801ddcc1
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-09-24 10:18:01 +0330

    create blog entity

---
+++ src/modules/blog/entities/blog.entity.ts
@@ -0,0 +1,39 @@
+import { BaseEntity } from "src/common/abstracts/base.entity";
+import { EntityName } from "src/common/enums/entity.enum";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  UpdateDateColumn,
+} from "typeorm";
+import { BlogStatus } from "../enum/status.enum";
+import { UserEntity } from "src/modules/user/entities/user.entity";
+
+@Entity(EntityName.Blog)
+export class BlogEntity extends BaseEntity {
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  content: string;
+
+  @Column({ nullable: true })
+  image: string;
+
+  @Column()
+  authorID: number;
+
+  @Column({ default: BlogStatus.Draft })
+  status: string;
+
+  // Date and time
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  update_at: Date;
+}
