278da082c7d1705fdb9ba02e2a85811ef96d540b
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-09-28 11:16:22 +0330

    store data in db

--- src/modules/blog/blog.service.ts
+++ src/modules/blog/blog.service.ts
@@ -1,21 +1,38 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable, Scope } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { BlogEntity } from "./entities/blog.entity";
 import { Repository } from "typeorm";
 import { CreateBlogDto } from "./dto/blog.dto";
 import { createSlug } from "src/common/utils/functions.util";
+import { BlogStatus } from "./enum/status.enum";
+import { REQUEST } from "@nestjs/core";
+import { Request } from "express";
 
-@Injectable()
+@Injectable({ scope: Scope.REQUEST })
 export class BlogService {
   constructor(
-    @InjectRepository(BlogEntity) private blogRepository: Repository<BlogEntity>
+    @InjectRepository(BlogEntity)
+    private blogRepository: Repository<BlogEntity>,
+    @Inject(REQUEST) private request: Request
   ) {}
 
   create(blogDto: CreateBlogDto) {
-    let { title, slug } = blogDto;
-    let slugData = slug ?? title;
-    blogDto.slug = createSlug(slugData);
+    const user = this.request.user;
 
+    let { title, slug, content, description, time_for_study, image } = blogDto;
+    let slugData = slug ?? title;
+    slug = createSlug(slugData);
+
+    const blog = this.blogRepository.create({
+      title,
+      slug,
+      content,
+      description,
+      time_for_study,
+      image,
+      status: BlogStatus.Draft,
+      authorID: user.id,
+    });
     return blogDto;
   }
 }
