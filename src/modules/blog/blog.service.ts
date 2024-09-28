5613e071eeada1b755b0713fe4280375fd27c2a6
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-09-28 12:54:36 +0330

    add random number at the end for duplicate slugs

--- src/modules/blog/blog.service.ts
+++ src/modules/blog/blog.service.ts
@@ -3,10 +3,11 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { BlogEntity } from "./entities/blog.entity";
 import { Repository } from "typeorm";
 import { CreateBlogDto } from "./dto/blog.dto";
-import { createSlug } from "src/common/utils/functions.util";
+import { createSlug, randomId } from "src/common/utils/functions.util";
 import { BlogStatus } from "./enum/status.enum";
 import { REQUEST } from "@nestjs/core";
 import { Request } from "express";
+import { PublicMessage } from "src/common/enums/message.enum";
 
 @Injectable({ scope: Scope.REQUEST })
 export class BlogService {
@@ -16,12 +17,16 @@ export class BlogService {
     @Inject(REQUEST) private request: Request
   ) {}
 
-  create(blogDto: CreateBlogDto) {
+  async create(blogDto: CreateBlogDto) {
     const user = this.request.user;
 
     let { title, slug, content, description, time_for_study, image } = blogDto;
     let slugData = slug ?? title;
     slug = createSlug(slugData);
+    const isExist = await this.checkBlogBySlug(slug);
+    if (isExist) {
+      slug += `-${randomId()}`;
+    }
 
     const blog = this.blogRepository.create({
       title,
@@ -33,6 +38,14 @@ export class BlogService {
       status: BlogStatus.Draft,
       authorID: user.id,
     });
-    return blogDto;
+
+    await this.blogRepository.save(blog);
+    return {
+      message: PublicMessage.Created,
+    };
+  }
+  async checkBlogBySlug(slug: string) {
+    const blog = await this.blogRepository.findOneBy({ slug });
+    return !!blog;
   }
 }
