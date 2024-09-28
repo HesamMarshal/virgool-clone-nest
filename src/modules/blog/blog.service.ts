431d3e1ebd1fc45ec46dd014435df46a336cb3ea
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-09-28 10:41:14 +0330

    create repository and create method

--- src/modules/blog/blog.service.ts
+++ src/modules/blog/blog.service.ts
@@ -1,4 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { BlogEntity } from "./entities/blog.entity";
+import { Repository } from "typeorm";
+import { CreateBlogDto } from "./dto/blog.dto";
 
 @Injectable()
-export class BlogService {}
+export class BlogService {
+  constructor(
+    @InjectRepository(BlogEntity) private blogRepository: Repository<BlogEntity>
+  ) {}
+
+  create(blogDto: CreateBlogDto) {
+    return blogDto;
+  }
+}
