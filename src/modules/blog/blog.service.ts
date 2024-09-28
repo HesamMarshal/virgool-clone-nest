3589fced4e24838e5082f04865a5a05e49ec093b
Hesam Marshal <14945056+HesamMarshal@users.noreply.github.com>
2024-09-28 11:02:35 +0330

    create slug for posts

--- src/modules/blog/blog.service.ts
+++ src/modules/blog/blog.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from "@nestjs/typeorm";
 import { BlogEntity } from "./entities/blog.entity";
 import { Repository } from "typeorm";
 import { CreateBlogDto } from "./dto/blog.dto";
+import { createSlug } from "src/common/utils/functions.util";
 
 @Injectable()
 export class BlogService {
@@ -11,6 +12,10 @@ export class BlogService {
   ) {}
 
   create(blogDto: CreateBlogDto) {
+    let { title, slug } = blogDto;
+    let slugData = slug ?? title;
+    blogDto.slug = createSlug(slugData);
+
     return blogDto;
   }
 }
